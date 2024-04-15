15f8105bba1b9cab64e34e4f263667c15d364126
rajatcodingwarrior21 <149108543+rajatcodingwarrior21@users.noreply.github.com>
2024-04-15 03:46:28 +0000

    datatypes of JS

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -3,8 +3,36 @@
 // alert( 3+3 )  //gives the ReferenceError as we are using Node.js not browser
 
 console.log( 3 + 3 ); console.log("Rajat");  // code readability low
+console.log( 3
+        +
+        3)
 
 console.log( 3 + 3 )
 console.log("Rajat")
 
 // code readability should be high
+
+let name = "Rajat" //string
+let age = 18 // number
+let isLoggedIn = false // boolean
+
+/* Data Types
+//number => 2 to power 53
+//bigint => used for website like trading , banking
+//string => "" or ''
+//boolean => true/false
+//null => stand alone value -->object type
+//undefined => when no value is assigned
+
+//symbol => uniqueness used in react , and figma 
+
+//object 
+
+*/
+
+console.log(typeof "Rajat")  // string
+console.log(typeof 22)  // number
+console.log(typeof 3.14)  // number
+console.log(typeof true) // boolean
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
