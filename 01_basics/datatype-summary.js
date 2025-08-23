597d0be1d8617cea6f4c794fafab2fe57f405fd2
rajatcodingwarrior21 <149108543+rajatcodingwarrior21@users.noreply.github.com>
2025-08-23 22:45:39 +0000

    memory heap and stack

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -1,45 +1,67 @@
-// Data types in JS are two type on basis of how is stored in memory and how is accessed -> by value or by reference
+// // Data types in JS are two type on basis of how is stored in memory and how is accessed -> by value or by reference
 
-// 1) Primitive datatypes (pass by value)=> 7 => number,boolean,bigint (n),null, undefined, symbol,string
+// // 1) Primitive datatypes (pass by value)=> 7 => number,boolean,bigint (n),null, undefined, symbol,string
 
-// const x="rajat"
-// console.log(typeof(x));
-// const y=23
-// console.log(typeof(y));
-// const z=22.45
-// console.log(typeof(z));
-// const t=32432432536454634424n
-// console.log(typeof(t));
-// const u='h'
-// console.log(typeof(u));
-// let id1=Symbol('124')
-// console.log(typeof(id1));
-// let id2=Symbol('124')
-// console.log(typeof(id2));
-// console.log(id1===id2);
-// let isGood=false
-// let temp=null
-// let email; // undefined type
+// // const x="rajat"
+// // console.log(typeof(x));
+// // const y=23
+// // console.log(typeof(y));
+// // const z=22.45
+// // console.log(typeof(z));
+// // const t=32432432536454634424n
+// // console.log(typeof(t));
+// // const u='h'
+// // console.log(typeof(u));
+// // let id1=Symbol('124')
+// // console.log(typeof(id1));
+// // let id2=Symbol('124')
+// // console.log(typeof(id2));
+// // console.log(id1===id2);
+// // let isGood=false
+// // let temp=null
+// // let email; // undefined type
 
-// 2) Reference type (Non Primitive) => array , objects , Functions
-// arrays
-let arr=["rajat",23,42];
-console.log(arr);
-console.log(typeof arr);
+// // 2) Reference type (Non Primitive) => array , objects , Functions
+// // arrays
+// let arr=["rajat",23,42];
+// console.log(arr);
+// console.log(typeof arr);
 
-let obj={
-    "id":123,
-    "name":"rajat"
-}
-console.log(obj);
-console.log(typeof obj);
+// let obj={
+//     "id":123,
+//     "name":"rajat"
+// }
+// console.log(obj);
+// console.log(typeof obj);
 
-let f=function(){
-    console.log("hi");
+// let f=function(){
+//     console.log("hi");
     
+// }
+// console.log(f());
+// console.log(typeof f);
+
+
+//*********************************** Memory ************************************************
+// stack and heap
+//stack ->primitive type (by value copy)
+// heap -> non primitive (by reference)
+
+let x="rajat";
+let y=x
+y="ram"
+console.log([x,y]);
+
+let userOne={
+    email:"rajat@gmail.com",
+    name:"raj"
 }
-console.log(f());
-console.log(typeof f);
+
+let userTwo=userOne;
+userTwo.email="khumbhu@gmail.com";
+
+console.log([userOne.email,userTwo.email]);
+
 
 
 
