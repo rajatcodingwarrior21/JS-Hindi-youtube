12cb560a6ddd251cdc1deff9108ef8ec7e8d88a2
rajatcodingwarrior21 <149108543+rajatcodingwarrior21@users.noreply.github.com>
2025-08-24 20:34:40 +0000

    math and numbers

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,53 @@
+// const n=200
+// const num= new Number(3000)
+// console.log(n);
+// console.log(num);
+
+// console.log(num.toString());
+// console.log(num.toString().length);
+// console.log(num.toFixed(2));
+
+// const otherNum=23.8921
+// console.log(otherNum.toPrecision(3));
+
+// //toLocaleString
+
+// const h=1000000
+// console.log(h.toLocaleString());
+// console.log(h.toLocaleString("en-IN"));
+
+
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+
+//************************************ Maths ************************************** */
+// sqrt
+//ceil
+//floor
+// pow
+// roundoff
+// random -(0-1)
+
+console.log(Math.sqrt(4));      // 2
+console.log(Math.ceil(4.1)); //// 5
+console.log(Math.floor(4.9));    // 4
+console.log(Math.abs(-4));     // 4
+console.log(Math.pow(2,3));   // 8
+console.log(Math.round(3.42));   // 3
+console.log(Math.random());   // any no between [0,1) 1 not included
+
+const min=10,max=35
+
+const val=min+(Math.floor(Math.random()*(max-min+1)));
+console.log(val);
+
+
+
+
+
+
+
+
+
+
+
