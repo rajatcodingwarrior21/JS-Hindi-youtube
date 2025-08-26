7cff6506056a56f421d561e9337b6bea0b938c66
rajatcodingwarrior21 <149108543+rajatcodingwarrior21@users.noreply.github.com>
2025-08-26 13:53:02 +0000

    scope basics

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,26 @@
+let x=10
+const y=20
+var z=30
+
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+if(true){
+    let x=200
+    const y=400
+    var z=100
+}
+
+
+console.log(x);
+console.log(y);
+console.log(z); 
+
+// z is not working in scope //
+// don't use var
+let array = [1 ,3,4]
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+// }
